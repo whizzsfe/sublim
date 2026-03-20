6c5afabc06daf92e4472420db2a9f54826ee64ff
InstrumentPurple <211818046+InstrumentPurple@users.noreply.github.com>
2026-03-20 15:54:16 -0600

    Add surpriserBird class with sound effects

--- sublim.py
+++ sublim.py
@@ -5,6 +5,9 @@ from PySide6.QtCore import QTimer,Slot
 from PySide6.QtTest import QTest
 from PySide6.QtCore import Qt
 
+from PySide6.QtMultimedia import QSoundEffect
+from PySide6.QtCore import QUrl
+
 import sys
 import random
 import os
@@ -38,6 +41,7 @@ class window(qt.QWidget):
         self.text.setFont(self.font)
 
         self.layout.addWidget(self.text,0,0)
+        self.setMinimumSize(1600, 52)
         self.tm.start()
         self.show()
 
@@ -83,7 +87,7 @@ class consiousReinforcement(qt.QWidget):
         self.text.setFont(self.font)
 
         self.layout.addWidget(self.text,0,0)
-
+        self.setMinimumSize(1600, 52)
         self.tm.start()
         self.show()
 
@@ -136,6 +140,7 @@ class surpriser(qt.QWidget):
         self.text.setFont(self.font)
 
         self.layout.addWidget(self.text,0,0)
+        self.setMinimumSize(1600, 52)
         self.tm.start()
         self.show()
 
@@ -163,6 +168,81 @@ class surpriser(qt.QWidget):
 
 
 
+class surpriserBird(qt.QWidget):
+    def __init__(self, fpath:str, isRandom: str, isburst: str):
+        super().__init__()
+        self.isRand = (isRandom == "random")
+        self.isBurst = (isburst == "burst")
+        with open(fpath, 'r') as file:
+            self.lines = file.readlines()
+
+        self.burstsWithoutBirds = 0
+        self.cur = 0
+        self.timeMs = 44 # adjust this if you want. 44 is at good timing tho. should be 34-50ms for it still to be subliminal
+
+        self.tm = QTimer()
+        self.tm.setInterval(self.timeMs)
+
+        self.tm.timeout.connect(self.changeText)
+
+        self.layout = qt.QGridLayout()
+        self.setLayout(self.layout)
+
+        self.text = qt.QLabel()
+        self.text.setAlignment(Qt.AlignHCenter)
+        self.font = QFont("Arial", 60)
+        self.text.setFont(self.font)
+
+
+        birdPaths = ["./bird1.wav","./bird2.wav", "./bird3.wav", "./bird4.wav"]
+        self.sounds = [QSoundEffect(),QSoundEffect(),QSoundEffect(),QSoundEffect()]
+        i = 0
+        for p in birdPaths:
+            self.sounds[i].setSource(QUrl.fromLocalFile(p))
+            self.sounds[i].setVolume(0.8)
+            i += 1
+
+        self.layout.addWidget(self.text,0,0)
+        self.setMinimumSize(1600, 52)
+        self.tm.start()
+        self.show()
+
+    @Slot()
+    def changeText(self ):
+        if self.isRand:
+            self.text.setText(self.lines[random.randrange(0, len(self.lines))])
+        else:
+            self.text.setText(self.lines[self.cur % len(self.lines)])
+
+        rnd = random.randrange(0,million)
+        if self.isBurst and (self.cur % len(self.lines) == 0):
+            QTest.qWait(self.timeMs)
+            self.text.setText("")
+
+            if self.burstsWithoutBirds >= 9.0: # been damn long enough just play one now
+                QTest.qWait(random.randrange(5000,14000))
+                random.choice(self.sounds).play()
+                QTest.qWait(100)
+                self.text.setText(self.lines[random.randrange(0, len(self.lines))])
+                QTest.qWait(self.timeMs)
+                self.text.setText("")
+                self.burstsWithoutBirds = 0
+            elif rnd < million / 6.0:
+                QTest.qWait(random.randrange(5000,14000))
+                random.choice(self.sounds).play()
+                QTest.qWait(100)
+                self.text.setText(self.lines[random.randrange(0, len(self.lines))])
+                QTest.qWait(self.timeMs)
+                self.text.setText("")
+            else:
+                self.burstsWithoutBirds += 1
+            QTest.qWait(random.randrange(6000,20000))
+
+
+        self.cur += 1
+
+
+
 
 if __name__=="__main__":
     app=qt.QApplication()
@@ -170,22 +250,18 @@ if __name__=="__main__":
     if len(sys.argv) >= 3:
         if sys.argv[2] == "rb":
             win=window(sys.argv[1], "random", "burst")
-            win.setMinimumSize(1600, 52)
         elif sys.argv[2] == "b":
             win=window(sys.argv[1], "", "burst")
-            win.setMinimumSize(1600, 52)
         elif sys.argv[2] == "r":
             win=window(sys.argv[1], "random", "")
-            win.setMinimumSize(1600, 52)
         elif sys.argv[2] == "c":
             win=consiousReinforcement(sys.argv[1], "random", "")
-            win.setMinimumSize(1600, 52)
         elif sys.argv[2] == "s":
             win=surpriser(sys.argv[1], "random", "burst")
-            win.setMinimumSize(1600, 52)
+        elif sys.argv[2] == "bird":
+            win=surpriserBird(sys.argv[1], "random", "burst")
     elif len(sys.argv) == 2:
         win=window(sys.argv[1], "", "")
-        win.setMinimumSize(1600, 52)
     else:
         print("USAGE: python ./sublim.py [text file] [options]")
         quit()
